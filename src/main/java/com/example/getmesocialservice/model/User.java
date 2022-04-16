bf0f4d108173490d4a15460404450d3d0e005137
rahuldagar <rahuldagar940@gmail.com>
2022-04-16 14:27:54 -0400

    First commit

---
+++ src/main/java/com/example/getmesocialservice/model/User.java
@@ -0,0 +1,48 @@
+package com.example.getmesocialservice.model;
+
+public class User
+{
+    private String name;
+    private String address;
+    private int age;
+    private String profilePicURL;
+
+    public User(String name, String address, int age, String profilePicURL) {
+        this.name = name;
+        this.address = address;
+        this.age = age;
+        this.profilePicURL = profilePicURL;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getProfilePicURL() {
+        return profilePicURL;
+    }
+
+    public void setProfilePicURL(String profilePicURL) {
+        this.profilePicURL = profilePicURL;
+    }
+}
---
+++ src/main/java/com/example/getmesocialservice/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.example.getmesocialservice.repository;
+
+import com.example.getmesocialservice.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository
+{
+    public User getUser()
+    {
+        //userRepository.getUser();
+        User user = new User("Rahul", "Canada", 65, "MyPC/Images");
+        return user;
+    }
+}
